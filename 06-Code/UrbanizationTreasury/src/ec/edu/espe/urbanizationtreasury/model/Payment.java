7492a97da086c7a1b851839a096da4c8c697fb85
Augusto Salazar <cesaraugustosalazar12345@gmail.com>
2022-11-16 00:11:09 -0500

    urbanizationTreasuryCode V1.0.0

---
+++ 06-Code/UrbanizationTreasury/src/ec/edu/espe/urbanizationtreasury/model/Payment.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.urbanizationtreasury.model;
+
+/**
+ *
+ * @author Augusto Salazar, WebMasterTeam, DCCO-ESPE
+ */
+public class Payment {
+    
+    private float aliquot;
+    private float accessCard;
+    private float accessControl;
+    private float extraordinary;
+
+    public float getAliquot() {
+        return aliquot;
+    }
+
+    public void setAliquot(float aliquot) {
+        this.aliquot = aliquot;
+    }
+
+    public float getAccessCard() {
+        return accessCard;
+    }
+
+    public void setAccessCard(float accessCard) {
+        this.accessCard = accessCard;
+    }
+
+    public float getAccessControl() {
+        return accessControl;
+    }
+
+    public void setAccessControl(float accessControl) {
+        this.accessControl = accessControl;
+    }
+
+    public float getExtraordinary() {
+        return extraordinary;
+    }
+
+    public void setExtraordinary(float extraordinary) {
+        this.extraordinary = extraordinary;
+    }
+    
+    
+    
+}
