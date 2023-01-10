259c8c58cc77c075b5de241e9dd8eb1e4564fa25
Matías Suárez Alexander Teran <matiasuarez_4b@hotmail.com>
2023-01-10 17:21:17 -0500

    UrbanizationTreasury_V2.0.0

---
+++ 06-Code/Urbanization Treasury v2.0.0/UrbanizationTreasurySystem/src/main/java/ec/edu/espe/urbanizationtreasury/view/UrbanizationTreasurySystem.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.urbanizationtreasury.view;
+
+/**
+ *
+ * @author Matias Suarez,WebMasterTeam,DCCO-ESPE 
+ */
+public class UrbanizationTreasurySystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
