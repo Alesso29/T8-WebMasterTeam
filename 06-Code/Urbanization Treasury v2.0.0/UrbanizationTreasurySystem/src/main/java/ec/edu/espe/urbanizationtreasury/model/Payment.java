3aefc459d408c5cb15cac1690358997d86bae7ba
Matías Suárez Alexander Teran <matiasuarez_4b@hotmail.com>
2023-01-10 17:31:36 -0500

    UrbanizationTreasury_V2.0.1

---
+++ 06-Code/Urbanization Treasury v2.0.0/UrbanizationTreasurySystem/src/main/java/ec/edu/espe/urbanizationtreasury/model/Payment.java
@@ -0,0 +1,28 @@
+package ec.edu.espe.urbanizationtreasury.model;
+
+/**
+ *
+ * @author Rivera Joel, WebMasterTeam, DCCO-ESPE
+ */
+public class Payment {
+    
+    private float payment;
+    private String month;
+
+    public float getPayment() {
+        return payment;
+    }
+
+    public void setPayment(float payment) {
+        this.payment = payment;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+    
+}
