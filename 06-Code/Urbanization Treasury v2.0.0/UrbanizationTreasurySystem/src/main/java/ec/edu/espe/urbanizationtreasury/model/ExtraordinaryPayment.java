f15b6f0678751644bf7f067706a1b5bbe2c80e49
Matías Suárez Alexander Teran <matiasuarez_4b@hotmail.com>
2023-01-18 02:11:01 -0500

    Urbanization_Treasury_System_V 2.6.1

--- 06-Code/Urbanization Treasury v2.0.0/UrbanizationTreasurySystem/src/main/java/ec/edu/espe/urbanizationtreasury/model/ExtraordinaryPayment.java
+++ 06-Code/Urbanization Treasury v2.0.0/UrbanizationTreasurySystem/src/main/java/ec/edu/espe/urbanizationtreasury/model/ExtraordinaryPayment.java
@@ -5,5 +5,7 @@ package ec.edu.espe.urbanizationtreasury.model;
  * @author Rivera Joel, WebMasterTeam, DCCO-ESPE
  */
 public class ExtraordinaryPayment extends Payment {
+
+
     
 }
--- 06-Code/Urbanization Treasury v2.0.0/UrbanizationTreasurySystem/src/main/java/ec/edu/espe/urbanizationtreasury/model/Person.java
+++ 06-Code/Urbanization Treasury v2.0.0/UrbanizationTreasurySystem/src/main/java/ec/edu/espe/urbanizationtreasury/model/Person.java
@@ -5,5 +5,48 @@ package ec.edu.espe.urbanizationtreasury.model;
  * @author Rivera Joel, WebMasterTeam, DCCO-ESPE
  */
 public class Person {
+  
+    private String name;
+    private long id;
+
+    public Person(String name, long id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + name + ", id=" + id + '}';
+    }
+    
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+    
     
 }
